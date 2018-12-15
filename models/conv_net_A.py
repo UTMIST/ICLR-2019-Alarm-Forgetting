887e148de78a89839b8263a565e723651fe54e5e
Yueze Fang <rex.fang@mail.utoronto.ca>
2018-12-15 00:26:11 -0500

    ignore the data downloaded, and try to train the network structure (does not seem to work...)

--- models/conv_net_A.py
+++ models/conv_net_A.py
@@ -1,4 +1,7 @@
 import torch
+import torch.nn
+import torch.optim
+import torch.autograd
 import torchvision
 
 # source: https://nextjournal.com/gkoehler/pytorch-mnist
@@ -45,3 +48,45 @@ batch_idx, (example_data, example_targets) = next(examples)
 
 # should output [ 1000, 1, 28, 28]
 print(example_data.shape)
+
+
+class MNISTNet(torch.nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.layer1_conv = torch.nn.Conv2d(1, 10, kernel_size=5)
+    self.activation1 = torch.nn.ReLU()
+    self.layer2_conv = torch.nn.Conv2d(10, 20, kernel_size=5)
+    self.activation2 = torch.nn.ReLU()
+    self.layer3_connected = torch.nn.Linear(20 * 20 * 20, 50)
+    self.activation3 = torch.nn.ReLU()
+    self.layer4_connected = torch.nn.Linear(50, 10)
+    self.activation4 = torch.nn.Softmax()
+
+  def forward(self, x):
+    y = self.activation1(self.layer1_conv(x))
+    y = self.activation2(self.layer2_conv(y))
+    y = y.view(y.size(0), -1)
+    y = self.activation3(self.layer3_connected(y))
+    y = self.activation4(self.layer4_connected(y))
+    return y
+
+
+def train_model(net, loader, epochs):
+  loss = torch.nn.CrossEntropyLoss()
+  optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
+  for epoch in range(0, epochs):
+    for i, (data, label) in enumerate(loader):
+        x, y = torch.autograd.Variable(data), torch.autograd.Variable(label)
+        optimizer.zero_grad()
+        y_pred = net(x)
+        loss_v = loss(y_pred, y)
+
+        print(loss_v, epoch, i)
+        loss_v.backward()
+        optimizer.step()
+
+
+if __name__ == "__main__":
+  # test the code
+  nn = MNISTNet()
+  train_model(nn, train_loader, n_epochs)
