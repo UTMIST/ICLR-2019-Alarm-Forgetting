c0825c9abde69a76407eb90c95d21fc40a4a6079
Yueze Fang <rex.fang@mail.utoronto.ca>
2018-12-15 17:00:51 -0500

    play around hyper parameters

--- models/conv_net_A.py
+++ models/conv_net_A.py
@@ -7,10 +7,10 @@ import torchvision
 # source: https://nextjournal.com/gkoehler/pytorch-mnist
 
 # set the hyperparameters
-n_epochs = 3
+n_epochs = 2
 batch_size_train = 64
 batch_size_test = 1000
-learning_rate = 0.01
+learning_rate = 0.1
 momentum = 0.5
 log_interval = 10
 
@@ -47,46 +47,52 @@ examples = enumerate(test_loader)
 batch_idx, (example_data, example_targets) = next(examples)
 
 # should output [ 1000, 1, 28, 28]
-print(example_data.shape)
+# print(example_data.shape)
 
 
 class MNISTNet(torch.nn.Module):
   def __init__(self):
     super().__init__()
-    self.layer1_conv = torch.nn.Conv2d(1, 10, kernel_size=5)
+    self.conv1 = torch.nn.Conv2d(1, 10, kernel_size=5, padding=2)
+    # self.pooling1 = torch.nn.MaxPool2d(5, padding=2)
     self.activation1 = torch.nn.ReLU()
-    self.layer2_conv = torch.nn.Conv2d(10, 20, kernel_size=5)
+    self.conv2 = torch.nn.Conv2d(10, 20, kernel_size=5, padding=2)
+    # self.pooling2 = torch.nn.MaxPool2d(5, padding=2)
     self.activation2 = torch.nn.ReLU()
-    self.layer3_connected = torch.nn.Linear(20 * 20 * 20, 50)
+    self.connected1 = torch.nn.Linear(28 * 28 * 20, 50)
+    # self.connected1 = torch.nn.Linear(2 * 2 * 20, 50)
     self.activation3 = torch.nn.ReLU()
-    self.layer4_connected = torch.nn.Linear(50, 10)
-    self.activation4 = torch.nn.Softmax()
+    self.connected2 = torch.nn.Linear(50, 10)
+    self.activation4 = torch.nn.Softmax(dim=1)
 
   def forward(self, x):
-    y = self.activation1(self.layer1_conv(x))
-    y = self.activation2(self.layer2_conv(y))
+    y = self.activation1(self.conv1(x))
+    y = self.activation2(self.conv2(y))
     y = y.view(y.size(0), -1)
-    y = self.activation3(self.layer3_connected(y))
-    y = self.activation4(self.layer4_connected(y))
+    y = self.activation3(self.connected1(y))
+    y = self.activation4(self.connected2(y))
     return y
 
 
 def train_model(net, loader, epochs):
+  # loss = torch.nn.NLLLoss()
   loss = torch.nn.CrossEntropyLoss()
   optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
   for epoch in range(0, epochs):
     for i, (data, label) in enumerate(loader):
-        x, y = torch.autograd.Variable(data), torch.autograd.Variable(label)
-        optimizer.zero_grad()
-        y_pred = net(x)
-        loss_v = loss(y_pred, y)
+      x, y = torch.autograd.Variable(data), torch.autograd.Variable(label)
+      optimizer.zero_grad()
+      y_pred = net(x)
+      loss_v = loss(y_pred, y)
 
-        print(loss_v, epoch, i)
-        loss_v.backward()
-        optimizer.step()
+      print(loss_v, epoch, i)
+      loss_v.backward()
+      optimizer.step()
 
 
 if __name__ == "__main__":
   # test the code
   nn = MNISTNet()
   train_model(nn, train_loader, n_epochs)
+  # print(nn(example_data))
+  # print(nn(example_data).shape, example_targets.shape, vector_form_label(example_targets).shape)
